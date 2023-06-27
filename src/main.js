3ff8d3d666d44b8c1d7777857f74a1ab5884bfe8
yangxun3 <yangxun3@lenovo.com>
2023-06-27 14:36:09 +0800

    init: qiankun-son

--- src/main.js
+++ src/main.js
@@ -3,11 +3,42 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './public-path'
 
 Vue.config.productionTip = false
+// 判断qiankun环境
+const __qiankun__ = window.__POWERED_BY_QIANKUN__
+console.log(111);
 
-new Vue({
+let instance;
+const render = (props={})=>{
+ const { container } = props
+ console.log('child:[render]',container);
+ instance = new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render:h=>h(App)
+ }).$mount(container ? container.querySelector('#app') : '#app')
+}
+
+
+
+export async function bootstrap() {
+  console.log('[vue] vue app bootstraped');
+}
+export async function mount(props) {
+  console.log('[vue] props from main framework', props);
+  render(props);
+}
+export async function unmount() {
+  instance.$destroy();
+  instance.$el.innerHTML = '';
+  instance = null;
+  router = null;
+}
+
+
+// 独立运行时
+if (!__qiankun__) {
+  render();
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const { name } = require('./package')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // publicPath:"/" + name,
+  transpileDependencies: true,
+  devServer:{
+    port:'8090',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      chunkLoadingGlobal: `webpackJsonp_${name}`,
+    },
+  }
 })
